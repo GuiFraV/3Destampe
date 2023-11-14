e63cc4ea6b77da884a288960cc45699533ebfb63
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-14 15:04:20 +0100

    5) 3d Models : add island 3d model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { Canvas } from "@react-three/fiber"
 import Loader from '../components/Loader';
+import {Island}  from '../models/Island';
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
     POPUP
@@ -18,6 +19,8 @@ function Home() {
                 <ambientLight />
                 <pointLight />
                 <hemisphereLight />
+
+                <Island />
             </Suspense>
 
         </Canvas>
