fcd0dfca2ac629ec12f47076e4001aa0b5b9c7f3
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-11-15 16:02:13 +0100

    3) Pages : fix bug  HomeInfo

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,51 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { arrow } from '../assets/icons'
+import { Link } from "react-router-dom";
 
-const InfoBox = ({ text, link, btnText }) => {
-    <div className='info-box'>
-        <p className='font-medium sm:text-xl text-center'>{text}</p>
-        {text}
-        <Link to={link} className='neo-brutalism-white neo-btn'>
-            {btnText}
-            <img src={arrow} className='w-4 h-4 object-contain'/>
-        </Link>
-    </div>
-}
-
-const renderContent = {
-    1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-           Hi, I am <span className='font-semibold'>Guillaume</span>🖐
-           <br/>
-           A Software Engineer from France
-        </h1>
-    ),
-    2: (
-        <InfoBox 
-            text="Worked with many companies and picked up many skills along the way"
-            link="/about"
-            btnText="Learn more about me"
-        />
-    ),
-    3: (
-        <InfoBox 
-            text="Led multiple projects to success over the years. Curious about the impact?"
-            link="/projects"
-            btnText="Visit my portefolio"
-        />
-    ),
-    4: (
-        <InfoBox 
-            text="Need a project done or looking for a dev? I'm just a few keystrokes away"
-            link="/contact"
-            btnText="Let's talk"
-        />
-    ),
-}
+import { arrow } from "../assets/icons";
 
 const HomeInfo = ({ currentStage }) => {
-  return renderContent[currentStage] || null;
-}
+  if (currentStage === 1)
+    return (
+      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+        Hi, I'm
+        <span className='font-semibold mx-2 text-white'>Adrian</span>
+        👋
+        <br />
+        A Software Engineer from Croatia 🇭🇷
+      </h1>
+    );
+
+  if (currentStage === 2) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium sm:text-xl text-center'>
+          Worked with many companies <br /> and picked up many skills along the way
+        </p>
+
+        <Link to='/about' className='neo-brutalism-white neo-btn'>
+          Learn more
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 3) {
+    return (
+      <div className='info-box'>
+        <p className='font-medium text-center sm:text-xl'>
+          Led multiple projects to success over the years. <br /> Curious about the impact?
+        </p>
+
+        <Link to='/projects' className='neo-brutalism-white neo-btn'>
+          Visit my portfolio
+          <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+        </Link>
+      </div>
+    );
+  }
+
+  if (currentStage === 4) {
+    return (
+      <div className='info-box'>
+      <p className='font-medium sm:text-xl text-center'>
+        Need a project done or looking for a dev? <br/> I'm just a few keystrokes away
+      </p>
+
+      <Link to='/contact' className='neo-brutalism-white neo-btn'>
+        Let's talk
+        <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+      </Link>
+    </div>
+    );
+  }
+
+  return null;
+};
 
 export default HomeInfo;
